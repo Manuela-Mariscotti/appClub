d258710b08cd8265d9a36a40d2d3ca62f17d9393
Manuela-Mariscotti <109516704+Manuela-Mariscotti@users.noreply.github.com>
2022-10-23 17:40:37 +0200

    Update clubs.client.js

--- appClubFront/clubs.client.js
+++ appClubFront/clubs.client.js
@@ -1,11 +1,1 @@
-class Club{
-    constructor(id_club,club_name,budget){
-        this.id_club = id_club;
-        this.club_name = club_name;
-        this.budget = budget;
-    }
-}
 
-function postClub(){
-    const url = "http://localhost:3000/club"
-} //----subir github antes de seguir
